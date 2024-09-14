02d4e5742b386adf977d2071015bf5616bb41128
mateus <mateusleguir@gmail.com>
2024-09-14 18:05:50 -0300

    atualizei ambiente,js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -9,3 +9,10 @@ do {
 //   console.log(`Passo ${c}`);
 //   c++;
 // }
+
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
