807027c72d756acaa962db7e99b8a5ba0008a0c6
mateus <mateusleguir@gmail.com>
2024-09-17 22:16:15 -0300

    criei funcao01.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,7 +4,7 @@ console.log(num);
 console.log(`O vetor tem ${num.length} posições.`);
 console.log(`O primeiro valor do vetor é ${num[0]}`);
 
-let pos = num.indexOf(4);
+let pos = num.indexOf(3);
 if (pos == -1) {
   console.log("O valor não foi encontrado!");
 } else {
